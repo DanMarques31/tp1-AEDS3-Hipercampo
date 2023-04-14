66711b27c334cba83491edac73dfcbddb6ca4670
DanMarques31 <daniellmarques2003@hotmail.com>
2023-04-14 20:29:35 -0300

    Add gettimeofday and getrusage

--- src/main.c
+++ src/main.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
+#include <sys/resource.h>
 #include "file.h"
 #include "hipercampo.h"
 
 int main(int argc, char *argv[]) {
     
+    struct timeval start, end;
+    struct rusage usage;
+
+    gettimeofday(&start, NULL);
+
     abre_arquivo(argv[2]);
 
     int qtdpontos = 0, anchorA = 0, anchorB = 0;
@@ -20,9 +27,17 @@ int main(int argc, char *argv[]) {
     
     int answer = math_func(pontos, qtdpontos, anchorA, anchorB);
 
-    printf("\n A quantidade maxima de pontos eh: %d\n", answer);
+    printf("\nA quantidade maxima de pontos eh: %d\n", answer);
     
     free(pontos);
     
     fecha_arquivo();
+
+    gettimeofday(&end, NULL);
+    getrusage(RUSAGE_SELF, &usage);
+
+    printf("Tempo de execução: %ld microsegundos\n", (((end.tv_sec - start.tv_sec) * 1000000) + (end.tv_usec - start.tv_usec)));
+    printf("Tempo de usuário: %ld microsegundos\n", usage.ru_utime.tv_usec);
+    printf("Tempo de sistema: %ld microsegundos\n", usage.ru_stime.tv_usec);
+
 }
