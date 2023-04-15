a3aff8e383d79f6a967c2ad95c1237d03d229c02
DanMarques31 <daniellmarques2003@hotmail.com>
2023-04-15 09:55:20 -0300

    Comentários

--- src/hipercampo.c
+++ src/hipercampo.c
@@ -3,12 +3,25 @@
 #include "file.h"
 #include "hipercampo.h"
 
+/*Essa função é responsável por obter do arquivo de entrada 
+os valores para a quantidade de pontos, âncora A e âncora B.*/
+
 void get_dados(int *qtdpontos, int *anchorA, int *anchorB) {
     
     *qtdpontos = scan_arquivo();
     *anchorA = scan_arquivo();
     *anchorB = scan_arquivo();
 }
+
+/*Essa seção do código faz várias verificações para determinar
+se o ponto j intersecta apenas nas âncoras, e se não intersecta
+em nenhum outro lugar da reta formada pelos pontos simétricos e rotacionados.
+A variável cont é inicializada com zero para cada ponto, e é usada para contar
+quantos outros pontos estão conectados ao ponto atual, onde a conexão é definida 
+como uma linha que só atravessa as âncoras. No final do loop interno, 
+o valor de cont é comparado com o valor atual de pconect (inicialmente zero) e,
+se for maior, pconect é atualizado com o novo valor. No final, a função retorna pconect,
+que é o número máximo de conexões para qualquer ponto no conjunto de pontos fornecido.*/
 
 int math_func(Pontos *pontos, int qtdpontos, int anchorA, int anchorB) {
     int pconect = 0;
@@ -43,7 +56,7 @@ int math_func(Pontos *pontos, int qtdpontos, int anchorA, int anchorB) {
                     continue;
                 }
 
-                // Verifica se o ponto j intersecta apenas nas âncoras
+                // Verifica se o ponto j intersecta apenas nas âncoras.
                 int intersecta_apenas_ancoras = 1;
                 for (int k = 0; k < qtdpontos; k++) {
                     if (k != i && k != j) {
--- src/main.c
+++ src/main.c
@@ -7,34 +7,56 @@
 
 int main(int argc, char *argv[]) {
     
+    //Cria as structs para o uso das funções 'gettimeofday' e 'getrusage' respectivamente.
+
     struct timeval start, end;
     struct rusage usage;
 
+    //Inicio da função 'gettimeofday'.
+
     gettimeofday(&start, NULL);
 
+    //Chamada da função abre_arquivo, simplesmente abre o arquivo e verifica se está NULL (vazio).
+
     abre_arquivo(argv[2]);
+
+    //Inicialização das variáveis e chamada da função get_dados,
+    //a qual obtém os valores das âncoras e da qtd de pontos.
 
     int qtdpontos = 0, anchorA = 0, anchorB = 0;
     get_dados(&qtdpontos, &anchorA, &anchorB);
 
+    //Aloca memória para o array de pontos e em seguida obtém os valores de x,y de cada ponto.
+
     Pontos *pontos;
     pontos = malloc(qtdpontos * sizeof(Pontos));
-    
+
     for (int i = 0; i < qtdpontos; i++) {
         pontos[i].x = scan_arquivo();
         pontos[i].y = scan_arquivo();
     }
     
+    //Cria o inteiro 'answer' para obter o retorno da função math_func,
+    //a qual retorna o valor da quantidade máxima de pontos que se intercectam apenas nas âncoras.
+
     int answer = math_func(pontos, qtdpontos, anchorA, anchorB);
 
     printf("\nA quantidade maxima de pontos eh: %d\n", answer);
     
+    //Liberação da memória alocada para os pontos (x,y).
+
     free(pontos);
     
+    //Fechamento do arquivo.
+
     fecha_arquivo();
+
+    //Fim da função 'gettimeofday' e uso da função 'getrusage'.
 
     gettimeofday(&end, NULL);
     getrusage(RUSAGE_SELF, &usage);
+
+    //Impressão dos tempos de execução.
 
     printf("Tempo de execução: %ld microsegundos\n", (((end.tv_sec - start.tv_sec) * 1000000) + (end.tv_usec - start.tv_usec)));
     printf("Tempo de usuário: %ld microsegundos\n", usage.ru_utime.tv_usec);
